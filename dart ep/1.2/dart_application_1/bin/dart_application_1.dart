c4747b5cca26f0127fda3ccae97609ae7939410a
KucherovPV <118096557+KucherovPV@users.noreply.github.com>
2022-11-22 12:07:38 +0700

    Delete dart ep directory

--- dart ep/1.2/dart_application_1/bin/dart_application_1.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-void main(List<String> arguments) {
-  var i = 1000;
-  var min = 3;
-  var max = 1000;
-  List<int> nums = new List.generate(100, (i) => i + 300 - (i * 4));
-  print(nums);
-}
