27739ab7dc5e7d2bc7b666a93cb2741bde048be5
KucherovPV <118096557+KucherovPV@users.noreply.github.com>
2022-11-22 23:51:16 +0700

    Update dart_application_1.dart

--- 1.3/dart_application_1/bin/dart_application_1.dart
+++ 1.3/dart_application_1/bin/dart_application_1.dart
@@ -8,38 +8,32 @@ void main(List<String> arguments) {
   List<String> lines = new File('input.txt').readAsLinesSync();
 
   List<String> ss = [];
-  for (var i = 0; i < lines.length; i++) {
-    print(lines[i]);
-    if (lines[i] != '') {
-      ss.add(lines[i]);
+
+  int amout = (int.parse(lines[1]));
+
+  List<String> temp = [];
+  temp += lines[0].split(' ');
+  List<int> winlist = [];
+  winlist = temp.map(int.parse).toList();
+  temp = [];
+
+  for (int i = 0; i < amout; i++) {
+    temp += lines[i + 2].split(' ');
+    List<int> ticket = temp.map(int.parse).toList();
+    temp = [];
+    check = 0;
+    for (int j = 0; j < winlist.length; j++) {
+      for (int k = 0; k < ticket.length; k++) {
+        if (ticket[k] == winlist[j]) {
+          check++;
+        }
+      }
+    }
+    if (check == 3) {
+      sink.write('Lucky\n');
+    } else {
+      sink.write('Unlucky\n');
     }
   }
-  print(ss);
-  // int amout = (int.parse(ss[1]));
-
-  // List<String> temp = [];
-  // temp += ss[0].split(' ');
-  // List<int> winlist = [];
-  // winlist = temp.map(int.parse).toList();
-  // temp = [];
-
-//   for (int i = 0; i < amout; i++) {
-//     temp += ss[i + 2].split(' ');
-//     List<int> ticket = temp.map(int.parse).toList();
-//     temp = [];
-//     check = 0;
-//     for (int j = 0; j < winlist.length; j++) {
-//       for (int k = 0; k < ticket.length; k++) {
-//         if (ticket[k] == winlist[j]) {
-//           check++;
-//         }
-//       }
-//     }
-//     if (check == 3) {
-//       sink.write('Lucky\n');
-//     } else {
-//       sink.write('Unlucky\n');
-//     }
-//   }
-//   sink.close();
+  sink.close();
 }
