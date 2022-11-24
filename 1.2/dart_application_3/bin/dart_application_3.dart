913994fb5d428a278bb383589dd8cf504f9b03dc
KucherovPV <118096557+KucherovPV@users.noreply.github.com>
2022-11-24 12:28:42 +0700

    Update dart_application_3.dart

--- 1.2/dart_application_3/bin/dart_application_3.dart
+++ 1.2/dart_application_3/bin/dart_application_3.dart
@@ -1,8 +1,11 @@
-import 'dart:math';
-
 void main(List<String> arguments) {
-  var nums = new List.generate(6, (i) => List.generate(6, (j) => 0 + 2));
-
+  var nums = new List.generate(6, (i) => List.generate(6, (j) => 0 + 1));
+  for (int i = 1; i < nums.length; i++) {
+    for (int j = 1; j < nums.length; j++) {
+      int sum = nums[i - 1][j] + nums[i][j - 1];
+      nums[i][j] = sum;
+    }
+  }
   nums.forEach((l) {
     print(l);
   });
