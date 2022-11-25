b58057d5d458fd07828d7c5406cabf60b092a06c
KucherovPV <118096557+KucherovPV@users.noreply.github.com>
2022-11-26 00:31:53 +0700

    Update dart_application_4.dart

--- 1.4/dart_application_4/bin/dart_application_4.dart
+++ 1.4/dart_application_4/bin/dart_application_4.dart
@@ -16,13 +16,12 @@ void main(List<String> arguments) {
   List<int> nums = ss.map(int.parse).toList();
   int sum = 0;
 
-  for (int i = 0; i < nums.length; i++) {
-    for (int j = i + 1; j < nums.length; j++) {
-      if (nums[i] == nums[j]) {
-        sum++;
-      }
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == nums[i + 1]) {
+      sum++;
     }
   }
+
   print(file);
-  print("количество одинаковых рядом стоящих чисел: $sum");
+  print("количество одинаковых рядом стоящих чисел: ${sum * 2}");
 }
